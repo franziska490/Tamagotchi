0f1a0dc575fb207f8d3d606081d42cbf078a70d8
Franziska G. <franziska.greunig@student.htl-rankweil.at>
2025-05-27 12:23:02 +0200

    Game over screen

--- scr/MyTamagotchi/MyTamagotchi/MainWindow.xaml.cs
+++ scr/MyTamagotchi/MyTamagotchi/MainWindow.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using MyTamagotchi.Models;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-
 using System.Threading.Tasks;
 
 namespace MyTamagotchi
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private Pet myPet;
@@ -27,39 +16,14 @@ namespace MyTamagotchi
         {
             InitializeComponent();
             myPet = selectedPet;
+
+            // Game Over überwachen
+            myPet.OnGameOver += ShowGameOverScreen;
+
             StartLifeTimer();
             UpdateUI();
-            //SelectPet();
-
-
         }
 
-        //private void SelectPet()
-        //{
-        //    PetSelectionWindow selectionWindow = new PetSelectionWindow();
-        //    if (selectionWindow.ShowDialog() == true)
-        //    {
-        //        string selectedType = selectionWindow.SelectedPetType;
-
-        //        switch (selectedType)
-        //        {
-        //            case "Seal":
-        //                myPet = new Pet("Chubby Seal");
-        //                PetImage.Source = new BitmapImage(new Uri("/Assets/seal_happy.png", UriKind.Relative));
-        //                break;
-        //            case "Pinguin":
-        //                myPet = new Pet("Sid");
-        //                PetImage.Source = new BitmapImage(new Uri("/Assets/seal_happy.png", UriKind.Relative));
-        //                break;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        myPet = new Pet("Seal");
-        //        PetImage.Source = new BitmapImage(new Uri("/Assets/seal_happy.png", UriKind.Relative));
-        //    }
-        //    UpdateUI();
-        //}
         private async Task LoadPet()
         {
             var pets = await PetApiService.GetPetsAsync();
@@ -81,54 +45,40 @@ namespace MyTamagotchi
             {
                 starter.UpdateImage();
                 PetImage.Source = starter.PetImage;
-
-                if (starter.Mood == 0 || starter.Energy == 0 || starter.Hunger == 0)
-                {
-                    lifeTimer.Stop();
-                }
             }
         }
 
-
-
-
-        // TASK THREADS
         private void StartLifeTimer()
         {
-            lifeTimer = new System.Windows.Threading.DispatcherTimer();
+            lifeTimer = new DispatcherTimer();
             lifeTimer.Interval = TimeSpan.FromSeconds(5);
             lifeTimer.Tick += LifeTimer_Tick;
             lifeTimer.Start();
         }
+
         private void LifeTimer_Tick(object sender, EventArgs e)
         {
-            //Haustier wird hungriger und müder
             myPet.Hunger = Math.Max(myPet.Hunger - 10, 0);
             myPet.Energy = Math.Max(myPet.Energy - 8, 0);
             myPet.Mood = Math.Max(myPet.Mood - 4, 0);
 
             UpdateUI();
-            //myPet.Hunger = Math.Max(myPet.Hunger - myPet.HungerDecreaseRate, 0);
-            //myPet.Energy = Math.Max(myPet.Energy - myPet.EnergyDecreaseRate, 0);
-            //myPet.Mood = Math.Max(myPet.Mood - myPet.MoodDecreaseRate, 0);
 
-            //UpdateUI();
+            myPet.CheckGameOver(); // sauberer direkter Aufruf
         }
+
         private void UpdateUI()
         {
             HungerBar.Value = myPet.Hunger;
             EnergyBar.Value = myPet.Energy;
             MoodBar.Value = myPet.Mood;
-            // Prozent gehen mit
+
             HungerValue.Text = $"{myPet.Hunger}%";
             EnergyValue.Text = $"{myPet.Energy}%";
             MoodValue.Text = $"{myPet.Mood}%";
 
-            UpdatePetImage(); // Automatische Bildauswahl
+            UpdatePetImage();
         }
-
-
-        // Buttons
 
         private async void FeedButton_Click(object sender, RoutedEventArgs e)
         {
@@ -175,12 +125,11 @@ namespace MyTamagotchi
             UpdateUI();
         }
 
-
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             await PetApiService.SavePetAsync(myPet);
-            // MessageBox.Show("Pet was saved!", "Info");
         }
+
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             PetSelectionWindow petSelectionWindow = new PetSelectionWindow();
@@ -188,7 +137,14 @@ namespace MyTamagotchi
             this.Close();
         }
 
-
+        private void ShowGameOverScreen()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                var gameOverWindow = new GameOverWindow();
+                gameOverWindow.Show();
+                this.Close(); // wichtig: MainWindow wird korrekt geschlossen
+            });
+        }
     }
 }
-
--- scr/MyTamagotchi/MyTamagotchi/Models/Pet.cs
+++ scr/MyTamagotchi/MyTamagotchi/Models/Pet.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
 
-
 namespace MyTamagotchi.Models
 {
     public class Pet
@@ -11,18 +10,14 @@ namespace MyTamagotchi.Models
         public int ownerid { get; set; }
 
         [JsonProperty("name")]
-        public string  Name{ get; set; }
+        public string Name { get; set; }
         [JsonProperty("hunger")]
-        public int Hunger { get; set; } 
+        public int Hunger { get; set; }
         [JsonProperty("energy")]
         public int Energy { get; set; }
         [JsonProperty("mood")]
         public int Mood { get; set; }
-        //public abstract string GetImagePath();
 
-       
-
-        // NEU: Sinkraten der Werte
         public int HungerDecreaseRate { get; set; } = 2;
         public int EnergyDecreaseRate { get; set; } = 2;
         public int MoodDecreaseRate { get; set; } = 1;
@@ -39,18 +34,32 @@ namespace MyTamagotchi.Models
         {
             Hunger = Math.Min(Hunger + 40, 100);
             Energy = Math.Max(Energy - 5, 0);
+            CheckGameOver();
         }
 
         public void Play()
         {
             Mood = Math.Min(Mood + 30, 100);
             Energy = Math.Max(Energy - 10, 0);
+            CheckGameOver();
         }
 
         public void Sleep()
         {
             Energy = Math.Min(Energy + 50, 100);
             Hunger = Math.Max(Hunger - 5, 0);
+            CheckGameOver();
         }
+
+        public event Action? OnGameOver;
+
+        public void CheckGameOver()
+        {
+            if (Hunger == 0 || Mood == 0 || Energy == 0)
+            {
+                OnGameOver?.Invoke();
+            }
+        }
+
     }
 }
